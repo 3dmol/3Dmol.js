7c35db6d5b45f861ccdfa72d5be91f2dbffa67d2
David Koes <dkoes@pitt.edu>
2014-08-07 15:45:31 -0400

    accidently turn on sync surface for everyone

--- build/webmol.js
+++ build/webmol.js
@@ -14987,7 +14987,7 @@ WebMol.multiLineString = function(f) {
 };
 
 //Synchronized (i.e. not threaded) surface gen? Used mainly for debugging
-WebMol.syncSurface = true;
+WebMol.syncSurface = false;
 
 // Internet Explorer refuses to allow webworkers in data blobs.  I can find
 // no way of checking for this feature directly, so must do a brower check
