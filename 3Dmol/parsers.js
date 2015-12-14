87d0447430c2e040fdb3c841e3306266613c5749
jus1in <justinj10000@gmail.com>
2015-12-14 14:24:56 -0500

    Rewrite assignBonds to be faster

--- 3Dmol/parsers.js
+++ 3Dmol/parsers.js
@@ -12,36 +12,97 @@ $3Dmol.Parsers = (function() {
      * @param {AtomSpec[]}
      *            atomsarray
      */
-    var assignBonds = function(atomsarray) {
+    var assignBonds = function(atoms) {
         // assign bonds - yuck, can't count on connect records
-        var atoms = atomsarray.slice(0);
-        var i, j, n;
-        for (i = 0, n = atomsarray.length; i < n; i++) {
+
+        for (var i = 0, n = atoms.length; i < n; i++) {
             // Don't reindex if atoms are already indexed
-            if (!atomsarray[i].index)
-                atomsarray[i].index = i;
+            if (!atoms[i].index)
+                atoms[i].index = i;
         }
 
-        atoms.sort(function(a, b) {
-            return a.z - b.z;
-        });
-        for (i = 0, n = atoms.length; i < n; i++) {
-            var ai = atoms[i];
+        var grid = {};
+        var MAX_BOND_LENGTH = 4.95; // (largest bond length, Cs) 2.25 * 2 * 1.1 (fudge factor)
 
-            for (j = i + 1; j < n; j++) {
-                var aj = atoms[j];
-                if (aj.z - ai.z > 4.725) // can't be connected
-                    break;
-                else if (Math.abs(aj.x - ai.x) > 4.725 || Math.abs(aj.y - ai.y) > 4.725) { // can't be connected either
-                    continue;
+        for (var index = 0; index < atoms.length; index++) {
+            var atom = atoms[index];
+            var x = Math.floor(atom.x / MAX_BOND_LENGTH);
+            var y = Math.floor(atom.y / MAX_BOND_LENGTH);
+            var z = Math.floor(atom.z / MAX_BOND_LENGTH);
+            if (!grid[x]) {
+                grid[x] = {};
+            }
+            if (!grid[x][y]) {
+                grid[x][y] = {};
+            }
+            if (!grid[x][y][z]) {
+                grid[x][y][z] = [];
+            }
+
+            grid[x][y][z].push(atom);
+        }
+
+        var findConnections = function(points, otherPoints) {
+            for (var i = 0; i < points.length; i++) {
+                var atom1 = points[i];
+                for (var j = 0; j < otherPoints.length; j++) {
+                    var atom2 = otherPoints[j];
+
+                    if (areConnected(atom1, atom2)) {
+                        atom1.bonds.push(atom2.index);
+                        atom1.bondOrder.push(1);
+                        atom2.bonds.push(atom1.index);
+                        atom2.bondOrder.push(1);
+                    }
                 }
-                else if (areConnected(ai, aj)) {
-                    if (ai.bonds.indexOf(aj.index) == -1) {
-                        // only add if not already there
-                        ai.bonds.push(aj.index);
-                        ai.bondOrder.push(1);
-                        aj.bonds.push(ai.index);
-                        aj.bondOrder.push(1);
+            }
+        }
+
+
+        /*const*/ var OFFSETS = [
+            {x: 0, y: 0, z: 1},
+            {x: 0, y: 1, z:-1},
+            {x: 0, y: 1, z: 0},
+            {x: 0, y: 1, z: 1},
+            {x: 1, y:-1, z:-1},
+            {x: 1, y:-1, z: 0},
+            {x: 1, y:-1, z: 1},
+            {x: 1, y: 0, z:-1},
+            {x: 1, y: 0, z: 0},
+            {x: 1, y: 0, z: 1},
+            {x: 1, y: 1, z:-1},
+            {x: 1, y: 1, z: 0},
+            {x: 1, y: 1, z: 1}
+        ];
+        for (var x in grid) {
+            x = parseInt(x);
+            for (var y in grid[x]) {
+                y = parseInt(y);
+                for (var z in grid[x][y]) {
+                    z = parseInt(z);
+                    var points = grid[x][y][z];
+
+                    for (var i = 0; i < points.length; i++) {
+                        var atom1 = points[i];
+                        for (var j = i + 1; j < points.length; j++) {
+                            var atom2 = points[j];
+                            if (areConnected(atom1, atom2)) {
+                                atom1.bonds.push(atom2.index);
+                                atom1.bondOrder.push(1);
+                                atom2.bonds.push(atom1.index);
+                                atom2.bondOrder.push(1);
+                            }
+                        }
+                    }
+
+                    for (var o = 0; o < OFFSETS.length; o++) {
+                        var offset = OFFSETS[o];
+                        if (!grid[x+offset.x]
+                            || !grid[x+offset.x][y+offset.y]
+                            || !grid[x+offset.x][y+offset.y][z+offset.z]) continue;
+
+                        var otherPoints = grid[x + offset.x][y + offset.y][z + offset.z];
+                        findConnections(points, otherPoints);
                     }
                 }
             }
