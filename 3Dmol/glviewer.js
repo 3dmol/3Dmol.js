8ee35f3840c943ee9db2404335c3b3df203518c9
David Koes <dkoes@pitt.edu>
2017-03-23 19:58:58 -0400

    fix bug with translate

--- 3Dmol/glviewer.js
+++ 3Dmol/glviewer.js
@@ -1334,7 +1334,8 @@ $3Dmol.GLViewer = (function() {
                         rotationGroup.quaternion,
                         final_position);
             } else { //no animation
-                camera.lookAt(final_position);
+                lookingAt = final_position;
+                camera.lookAt(lookingAt);
                 show();
             }
             return this;
