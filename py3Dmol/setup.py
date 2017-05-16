540e2feb44d2c85e3b17858d53350a51ee098ea5
David Koes <dkoes@pitt.edu>
2017-05-16 16:53:00 -0400

    bump version

--- py3Dmol/setup.py
+++ py3Dmol/setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.6.2',
+    version='0.6.3',
 
     description='An IPython interface for embedding 3Dmol.js views',
     long_description=long_description,
