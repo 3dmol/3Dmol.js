c6d10042a9fe8eb9eaa7d793190bb1a769916c31
Manohar-94 <khmanohar3@gmail.com>
2017-08-22 16:47:50 -0500

    added some functions to adjust the stereo rendering

--- tests/auto/tests/test96.js
+++ tests/auto/tests/test96.js
@@ -4,13 +4,15 @@
 
     $.get("volData/TC5b.prmtop",
         function(data) {
-
             var models = stereoViewer.addModel(data, "prmtop");
             $.get("volData/heat1.mdcrd", 
             function(data) {
                 stereoViewer.setCoordinates(models, data, "mdcrd");
                 stereoViewer.setStyle({},{sphere:{}});
                 stereoViewer.zoomTo();
+                var dist = stereoViewer.getPerceivedDistance();
+                stereoViewer.setPerceivedDistance(dist[0]-10); //useful for zooming in or out
+                stereoViewer.setAutoEyeSeparation(); //optional. changes the camera x value
                 stereoViewer.animate({loop:"forward",reps:1});
                 stereoViewer.render(callback);
             });
