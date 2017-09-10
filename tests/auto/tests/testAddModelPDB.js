aeaebdca105466c80698915d73cd5418c48909b8
David Koes <dkoes@pitt.edu>
2017-09-09 20:45:09 -0400

    fix tests

--- tests/auto/tests/testAddModelPDB.js
+++ tests/auto/tests/testAddModelPDB.js
@@ -4,6 +4,6 @@ $.get('volData/4csv.pdb', function(data) {
   viewer.addModel(data,'pdb');
   viewer.setStyle({cartoon:{},stick:{}});
   viewer.zoomTo();
-  viewer.render( /*no callback*/);
+  viewer.render();
   
 });
