72c87754c373a4f81c26af8ca55c4def206ff087
David Koes <dkoes@pitt.edu>
2018-06-27 17:32:09 -0400

    ref img

--- tests/auto/tests/test_ccp4.js
+++ tests/auto/tests/test_ccp4.js
@@ -3,9 +3,7 @@
 $.get('data/1lo6.pdb', function(data) {
       viewer.addModel(data,'pdb');
       viewer.setStyle({cartoon:{},stick:{}});
-      viewer.zoomTo();
-      viewer.render( /*no callback*/);
-    
+
     //can't use jquery with binary data
     $3Dmol.getbin('data/1lo6_2FOFC.ccp4', function(data) {     
        var voldata = new $3Dmol.VolumeData(data, 'ccp4');
