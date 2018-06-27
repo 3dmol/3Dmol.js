16ba59a6b9ed2142acc16f6467005b4286c7c8fc
David Koes <dkoes@pitt.edu>
2018-06-27 17:45:21 -0400

    like this version better

--- tests/auto/tests/test_ccp4.js
+++ tests/auto/tests/test_ccp4.js
@@ -9,11 +9,16 @@ $.get('data/1lo6.pdb', function(data) {
        var voldata = new $3Dmol.VolumeData(data, 'ccp4');
       viewer.addIsosurface(voldata, {isoval: 0.1,
                                        color: "green",
-                                       opacity: .8,
+                                       opacity: .5,
                                        coords: viewer.selectedAtoms({resi: 18}),
                                        seldist: 3.0                                   
                                     });
-                                                     
+      viewer.addIsosurface(voldata, {isoval: 0.3,
+                                       color: "blue",
+                                       wireframe: true,
+                                       coords: viewer.selectedAtoms({resi: 18}),
+                                       seldist: 3.0                                   
+                                    });                                                     
       viewer.zoomTo({resi: 18});
       viewer.render();
     });
