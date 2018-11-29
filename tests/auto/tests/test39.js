1374e8ae6f4c8d9117cf8582a376b05901d3e320
David Koes <dkoes@pitt.edu>
2018-11-29 15:43:31 -0500

    fix test (updated pdb code)

--- tests/auto/tests/test39.js
+++ tests/auto/tests/test39.js
@@ -1,6 +1,6 @@
              
               var m1 = $3Dmol.download("pdb:2V0E",viewer,{multimodel:true, frames:true},function(){
-                var m2 = $3Dmol.download("pdb:2MA0",viewer,{multimodel:true, frames:true},function(){
+                var m2 = $3Dmol.download("pdb:5ZB6",viewer,{multimodel:true, frames:true},function(){
                   
                   m1.setStyle({}, {stick:{color:"red"}});
                   m2.setStyle({}, {cartoon:{color:"blue"}});
