59984e85fa9482da7a5756ebc9c664b81a81e327
David Koes <dkoes@pitt.edu>
2019-07-10 23:55:37 -0400

    Fix removeLabel documentation

--- 3Dmol/glviewer.js
+++ 3Dmol/glviewer.js
@@ -1886,11 +1886,11 @@ $3Dmol.GLViewer = (function() {
          * 
          * @example // Remove labels created in 
          $3Dmol.download("pdb:2EJ0",viewer,{},function(){
-         *    viewer.addLabel("Aromatic", {position: {x:-6.89, y:0.75, z:0.35}, backgroundColor: 0x800080, backgroundOpacity: 0.8});
+                  var toremove = viewer.addLabel("Aromatic", {position: {x:-6.89, y:0.75, z:0.35}, backgroundColor: 0x800080, backgroundOpacity: 0.8});
                   viewer.addLabel("Label",{font:'sans-serif',fontSize:18,fontColor:'white',fontOpacity:1,borderThickness:1.0,
                                            borderColor:'red',borderOpacity:0.5,backgroundColor:'black',backgroundOpacity:0.5,
                                            position:{x:50.0,y:0.0,z:0.0},inFront:true,showBackground:true});
-                  viewer.remove
+                  viewer.removeLabel(toremove);
                   viewer.render();
 
                   
