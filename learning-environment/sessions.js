4c9679b3b6d3e3337bc4fae6ee27bb90e4cd2db2
David Koes <dkoes@pitt.edu>
2019-07-15 09:35:38 -0400

    Special case running on localhost

--- learning-environment/sessions.js
+++ learning-environment/sessions.js
@@ -15,7 +15,12 @@ var initSessions = function() {
 
     //webserver needs to have appropriate rules to forward to flask
     //https://stackoverflow.com/questions/36472920/apache-proxy-configuration-for-socket-io-project-not-in-root
-    socket = io.connect(window.location.hostname);
+    if(window.location.hostname == 'localhost') {
+      //for debugging on localhost go straight to port to avoid having to setup webserver
+      socket = io.connect(window.location.hostname+":5000");
+    } else {
+      socket = io.connect(window.location.hostname);
+    }
     socket.on('connect', function() {
         socket.send('User has connected!');
     });
