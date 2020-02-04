7413c336bf4106a154017c60e50d83888ae116b9
David Koes <dkoes@pitt.edu>
2020-02-04 14:41:24 -0500

    fixup tests

--- tests/auto/tests/test19.js
+++ tests/auto/tests/test19.js
@@ -2,10 +2,10 @@ var setStyles = function(volumedata){
 	var data = new $3Dmol.VolumeData(volumedata, "cube");
 	viewer.zoomTo({chain:'A'});
         viewer.rotate(-110);
-	viewer.render( /*no callback, why? */);
+
         viewer.addSurface("VDW", {opacity:0.85, voldata: data, volscheme: new $3Dmol.Gradient.RWB(-10,10)},{chain:'A'});
 	//make sure we see the volume mapped color
-        viewer.render( /*no callback */);
+        viewer.render();
 };
 $3Dmol.download("pdb:4DLN",viewer,{},function(){
 	$.get("data/1fas.cube",setStyles);
