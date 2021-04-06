453f6960f0ec48b3bd56aa192f105332b4504849
dkoes <dkoes@pitt.edu>
2021-04-06 08:32:41 -0400

    test image
    also fix dropbox link

--- tests/auto/tests/testallselsurf.js
+++ tests/auto/tests/testallselsurf.js
@@ -5,4 +5,7 @@ $3Dmol.download("pdb:1YCR",viewer,{},function(){
                   viewer.setStyle({chain:'B'},{'cartoon':{color:'blue'}});
                   viewer.addSurface("SAS", {opacity:0.85, color:'white'},{chain:'A',within:{distance:5,sel:{chain:'B'}}},{chain:'A'});
                   viewer.rotate(90);
+	    viewer.zoomTo();
+    viewer.render();
+
               });
