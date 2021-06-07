7bf626d7cec57f54beaa32895ebb8cc51b67d353
Jai Shukla <jay@live.co.in>
2021-06-08 00:54:31 +0530

    Setting up outline for the 3dmol.js

---
+++ 3Dmol/ui/state.js
@@ -0,0 +1,25 @@
+// @param {startState} object it store the initial values to initiliaze the
+// UI state
+// Everytime UI state is changed this state is updated so that this can
+// preserve the changes in case error happens
+
+// This will also help in migrating the controls from to different
+
+$3Dmol.UI.States = (function(){
+  function State(startState){
+    this.position.x ;
+    this.position.y ; //=
+    this.height; // =
+    this.width ; //=
+    this.viewerState ; //=
+    this.viewToolbar ; //=
+    this.viewSidebar ; //=
+    this.viewDialogBox ; //=
+    this.viewAnimationControl ; //=
+    this.viewSelectionBox ; //=
+    this.viewStyleBox ; //=
+    this.controlTyle ; //=
+    this.toolTip ; //=
+
+  }
+})()
---
+++ 3Dmol/ui/ui.js
@@ -0,0 +1,104 @@
+/*
+* This is ui that we are trying to draw at the time of generationo
+* of viewer.
+*/
+
+$3Dmol.UI = (function(){
+  function UI(element, config){
+    // Extract the viewer and then render it
+    var container = $(element);
+
+    // High level elements
+    function preferences(){
+
+    }
+
+    function toolbar(){
+      var toolbar = $('<div></div>');
+      toolbar.text()
+    }
+
+    function styleList(){
+
+    }
+
+    function selectionList(){
+
+    }
+
+    function dialog(){
+
+    }
+
+    function contextMenu() {
+
+    }
+
+    function sideOptionBar() {
+
+    }
+
+    function animationControls() {
+
+    }
+
+    // Medium Level elements
+
+    function button(){
+
+    }
+
+    function searchBar() {
+
+    }
+
+    function orderedList() {
+
+    }
+
+    function unorderedList() {
+
+    }
+
+    function dropdownList() {
+
+    }
+
+    function input() {
+
+    }
+
+    function formFromSpec() {
+
+    }
+
+    // Lower Level elements
+
+    function position() {
+
+    }
+
+    function colors() {
+
+    }
+
+    function borders() {
+
+    }
+
+    function cssColors() {
+
+    }
+
+    function gradients() {
+
+    }
+
+    function zIndex(){
+
+    }
+
+  }
+
+  return UI;
+})();
