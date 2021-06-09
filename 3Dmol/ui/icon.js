b33825dc0178655dc4fd3acf556a0c66fd543c56
jaxmatrix <jaishukla7768@gmail.com>
2021-06-09 06:53:40 +0530

    Added button; Working on Selection and Style boxes

--- 3Dmol/ui/icon.js
+++ 3Dmol/ui/icon.js
@@ -5,21 +5,10 @@
 
 $3Dmol.UI.Icons = (function(){
   function Icons(){
-    this.move = `<svg version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-    viewBox="0 0 32 32" style="enable-background:new 0 0 32 32;" xml:space="preserve">
-    <style type="text/css">
-    .st0{fill:none;stroke:#000000;stroke-width:2;stroke-miterlimit:10;}
-    .st1{fill:none;stroke:#000000;stroke-width:2;stroke-linejoin:round;stroke-miterlimit:10;}
-    .st2{fill:none;stroke:#000000;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
-    .st3{fill:none;stroke:#000000;stroke-width:2;stroke-linecap:round;stroke-miterlimit:10;}
-    .st4{fill:none;stroke:#000000;stroke-width:2;stroke-linejoin:round;stroke-miterlimit:10;stroke-dasharray:3;}
-    </style>
-    <path class="st2" d="M15,17V7c0-1.1-0.9-2-2-2h0c-1.1,0-2,0.9-2,2v13l-3-5.2c-0.6-1-1.8-1.3-2.8-0.6l0,0c-0.7,0.5-1.1,1.4-0.7,2.3
-    l0,0c1.6,4.3,4,8.4,7,11.9"/>
-    <path class="st2" d="M15,17V5c0-1.1,0.9-2,2-2h0c1.1,0,2,0.9,2,2v12"/>
-    <path class="st2" d="M19,17V7c0-1.1,0.9-2,2-2h0c1.1,0,2,0.9,2,2v10"/>
-    <path class="st2" d="M23,17V9c0-1.1,0.9-2,2-2h0c1.1,0,2,0.9,2,2v8v0c0,4-0.8,7.9-2.4,11.5"/>
-    </svg>`;
+    this.move = `<svg  id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32"  xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <path d="M31.338,14.538L27.38,10.58C26.994,10.193,26.531,10,26,10c-1.188,0-2,1.016-2,2c0,0.516,0.186,0.986,0.58,1.38L25.2,14H18  V6.8l0.62,0.62C19.014,7.814,19.484,8,20,8c0.984,0,2-0.813,2-2c0-0.531-0.193-0.994-0.58-1.38l-3.973-3.974  C17.08,0.279,16.729,0,16,0s-1.135,0.334-1.463,0.662L10.58,4.62C10.193,5.006,10,5.469,10,6c0,1.188,1.016,2,2,2  c0.516,0,0.986-0.186,1.38-0.58L14,6.8V14H6.8l0.62-0.62C7.814,12.986,8,12.516,8,12c0-0.984-0.813-2-2-2  c-0.531,0-0.994,0.193-1.38,0.58l-3.958,3.958C0.334,14.866,0,15.271,0,16s0.279,1.08,0.646,1.447L4.62,21.42  C5.006,21.807,5.469,22,6,22c1.188,0,2-1.016,2-2c0-0.516-0.186-0.986-0.58-1.38L6.8,18H14v7.2l-0.62-0.62  C12.986,24.186,12.516,24,12,24c-0.984,0-2,0.813-2,2c0,0.531,0.193,0.994,0.58,1.38l3.957,3.958C14.865,31.666,15.271,32,16,32  s1.08-0.279,1.447-0.646l3.973-3.974C21.807,26.994,22,26.531,22,26c0-1.188-1.016-2-2-2c-0.516,0-0.986,0.186-1.38,0.58L18,25.2V18  h7.2l-0.62,0.62C24.186,19.014,24,19.484,24,20c0,0.984,0.813,2,2,2c0.531,0,0.994-0.193,1.38-0.58l3.974-3.973  C31.721,17.08,32,16.729,32,16S31.666,14.866,31.338,14.538z"/>
+    </svg>
+    `;
 
 
     this.rotate = `<svg version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
@@ -34,6 +23,8 @@ $3Dmol.UI.Icons = (function(){
     <path class="st0" d="M11,22.5c-0.3-0.2-0.5-0.5-0.8-0.8c-7-7-10.2-15.3-7-18.5s11.5-0.1,18.5,7s10.2,15.3,7,18.5
     c-1.7,1.7-4.8,1.6-8.4,0.1"/>
     </svg>`;
+
+    this.option = ` <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><defs><style>.cls-1{fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:2px;}</style></defs><title>69.option</title><g id="_69.option" data-name="69.option"><rect class="cls-1" x="1" y="1" width="9" height="9" rx="2" ry="2"/><rect class="cls-1" x="14" y="1" width="9" height="9" rx="2" ry="2"/><rect class="cls-1" x="14" y="14" width="9" height="9" rx="2" ry="2"/><rect class="cls-1" x="1" y="14" width="9" height="9" rx="2" ry="2"/></g></svg>`;
   }
 
   return Icons;
--- 3Dmol/ui/ui.js
+++ 3Dmol/ui/ui.js
@@ -17,20 +17,17 @@ $3Dmol.UI = (function(){
     // Extract the viewer and then render it
     var container = $(element);
 
+
+    // Generates the necessary UI elements
+    var icons =new $3Dmol.UI.Icons();
     addToViewer();
 
     function addToViewer(){
-      var topbar = toolbar();
+      var topbar =new Toolbar();
       $('body').append(topbar);
-
-      var icons =new $3Dmol.UI.Icons();
-      $('body').append(icons.rotate);
+      var selectionBox = new SelectionBox();
+      $('body').append(selectionBox);
     }
-    // High level elements
-    function preferences(){
-
-    }
-
 
     /**
     * function to make the html toolbar div
@@ -39,11 +36,17 @@ $3Dmol.UI = (function(){
     * @param width
     * @param height
     **/
-    function toolbar(){
+    function Toolbar(){
       var toolbar = $('<div></div>');
 
       // Toolbar content
-      toolbar.text('This is cool');
+
+      var optionButton = new button_toolbar(icons.option, 20)
+      var moveButton = new button_toolbar(icons.move, 20);
+      var rotateButton = new button_toolbar(icons.rotate, 20);
+      toolbar.append(optionButton);
+      toolbar.append(moveButton);
+      toolbar.append(rotateButton);
 
       // Toolbar design
       toolbar.css('position', 'absolute');
@@ -53,12 +56,106 @@ $3Dmol.UI = (function(){
       toolbar.css('color', 'white');
       toolbar.css('width', '300px');
 
-
-
       return toolbar;
     }
 
-    function toolbar
+    /**
+     * button - generates button with the given markup as contents
+     * @param {string} SVG markup string that contains the content of the button
+     * @param {number} Height of the content
+     * @param {}
+     * @return {object}  Returns the jquery object for button
+     */
+    function button_toolbar(svg, height){
+      // Button instance
+      var button = $('<div></div>');
+
+      // CSS
+      button.css('box-sizing', 'border-box');
+      button.css('display', 'inline-block');
+      button.css('margin-right', '5px');
+      button.css('height', height);
+      button.css('width', height);
+      button.css('border-radius', (height/2) + 'px');
+      button.css('padding', '3px');
+      button.css('color', 'black');
+      button.css('background', 'rgb(177, 194, 203)');
+
+      // content
+      var formatted_content = $(svg).height(height-6);
+      button.append(formatted_content);
+
+      return button;
+    }
+
+
+    /**
+     * SelectionBox - Draws the box where all the selections on atoms are listed
+     * This will be used to modify style for specific set of selection
+     *
+     * @return {Object}  Jquery element of div
+     */
+    function SelectionBox() {
+      var selectionBox = $('<div></div>')
+
+      // Content
+
+      // CSS
+      selectionBox.css('box-sizing', 'border-box');
+      selectionBox.css('padding', '3px');
+      selectionBox.css('width', '120px');
+      selectionBox.css('height', '300px');
+      selectionBox.css('border-radius', '6px');
+      selectionBox.css('background', 'rgb(214, 214, 214)');
+
+      // CSS Position
+      selectionBox.css('position','absolute');
+      // selectionBox.css('top', '0px');
+      // selectionBox.css('left', '0px');
+
+      // Selection Box modification function
+
+      /**
+       * Add a selection input to the list
+       */
+      this.addSelection = function(ParsedSelectionObject){
+
+      }
+
+      return selectionBox;
+    }
+
+
+    /**
+     * parseSelection - generate markup using the selection list
+     *
+     * @param {list} Takes in the list of objects that are to selected
+     * @return {Objects}  Returns list of selection after preprocessing the
+     * SelectionSpec and StyleSpec. This is called `ParsedSelectionObject`
+     */
+    function parseSelection(Selections){
+      return [
+        { 'selection-name': 'Name 1',
+          'properties':[
+            { 'name':'property1', 'value':'value1', 'type':'input' },
+            { 'name':'property2', 'value':'value2', 'type':'radio' },
+            { 'name':'property3', 'value':'value3', 'type':'checkbox' },
+            { 'name':'property4', 'value':'value4', 'type':'list' },
+          ],
+          'style' : { 'name':'StyleType1', 'value':'value1', 'type':'input' }
+        },
+        { 'selection-name': 'Name 2',
+          'properties':[
+            { 'name':'property1', 'value':'value1', 'type':'input' },
+            { 'name':'property2', 'value':'value2', 'type':'radio' },
+            { 'name':'property3', 'value':'value3', 'type':'checkbox' },
+            { 'name':'property4', 'value':'value4', 'type':'list' },
+          ],
+          'style' : { 'name':'StyleType1', 'value':'value1', 'type':'input' }
+        },
+      ];
+    }
+
 
 
   }
