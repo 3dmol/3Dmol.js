de845b9722147713b6e628baa2aa9e1453dc3ab4
Zirui Huang <472383277@qq.com>
2022-03-12 16:09:30 -0500

    change != to !==

--- 3Dmol/SurfaceWorker.js
+++ 3Dmol/SurfaceWorker.js
@@ -1,17 +1,17 @@
-//Hackish way to create webworker (independent of $3Dmol namespace) within minified file
+// Hackish way to create webworker (independent of $3Dmol namespace) within minified file
 $3Dmol.workerString = function(){
 
     self.onmessage = function(oEvent) {
-        var obj = oEvent.data;
-        var type = obj.type;
+        const obj = oEvent.data;
+        const {type} = obj;
         if (type < 0) // sending atom data, initialize
         {
             self.atomData = obj.atoms;
             self.volume = obj.volume;
             self.ps = new ProteinSurface();  // jshint ignore:line
         } else {
-            var ps = self.ps;
-            ps.initparm(obj.expandedExtent, (type == 1) ? false : true, self.volume);
+            const {ps} = self;
+            ps.initparm(obj.expandedExtent, type != 1, self.volume);
             ps.fillvoxels(self.atomData, obj.extendedAtoms);
             ps.buildboundary();
             if (type === 4 || type === 2) {
@@ -20,7 +20,7 @@ $3Dmol.workerString = function(){
                 ps.fillvoxelswaals(self.atomData, obj.extendedAtoms);    
             }        
             ps.marchingcube(type);
-            var VandF = ps.getFacesAndVertices(obj.atomsToShow);
+            const VandF = ps.getFacesAndVertices(obj.atomsToShow);
             self.postMessage(VandF);
         }
     };
@@ -29,10 +29,10 @@ $3Dmol.workerString = function(){
 
 // NOTE: variable replacement is simplified
 // (See: http://stackoverflow.com/questions/1661197/what-characters-are-valid-for-javascript-variable-names)
-$3Dmol.workerString += "; var ProteinSurface=" + $3Dmol.ProteinSurface.toString().replace(/[a-zA-Z_$]{1}[0-9a-zA-Z_$]*.MarchingCube./g, "MarchingCube.");
-$3Dmol.workerString += ",MarchingCube=("+$3Dmol.MarchingCubeInitializer.toString() +")();";
+$3Dmol.workerString += `; var ProteinSurface=${  $3Dmol.ProteinSurface.toString().replace(/[a-zA-Z_$]{1}[0-9a-zA-Z_$]*.MarchingCube./g, "MarchingCube.")}`;
+$3Dmol.workerString += `,MarchingCube=(${$3Dmol.MarchingCubeInitializer.toString() })();`;
 
-$3Dmol.SurfaceWorker = window.URL ? window.URL.createObjectURL(new Blob([$3Dmol.workerString],{type: 'text/javascript'})) : {postMessage:function(){}};
+$3Dmol.SurfaceWorker = window.URL ? window.URL.createObjectURL(new Blob([$3Dmol.workerString],{type: 'text/javascript'})) : {postMessage(){}};
 
 $3Dmol.workerString = $3Dmol.workerString;
 $3Dmol.SurfaceWorker = $3Dmol.SurfaceWorker;
