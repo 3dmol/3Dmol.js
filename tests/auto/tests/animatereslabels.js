74ce8e0642b19a80d279546a99cef90c505c94d7
Zirui Huang <472383277@qq.com>
2022-03-18 01:13:41 -0400

    eslint auto fix

--- tests/auto/tests/animatereslabels.js
+++ tests/auto/tests/animatereslabels.js
@@ -1,6 +1,6 @@
 
     
-$.get('data/temp_1_2_28.pdb', function(data) {
+$.get('data/temp_1_2_28.pdb', (data) => {
     viewer.addModelsAsFrames(data,'pdb');
     viewer.setStyle({cartoon:{},stick:{}});
     viewer.addResLabels({'resn':['ADE','THY','CYT','GUA']},{},true);
