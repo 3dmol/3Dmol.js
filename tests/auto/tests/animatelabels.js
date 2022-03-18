7401fd542206f61a4745af81a1b1b22a07cd33c9
Zirui Huang <472383277@qq.com>
2022-03-18 01:12:01 -0400

    eslint auto fix

--- tests/auto/tests/animatelabels.js
+++ tests/auto/tests/animatelabels.js
@@ -1,6 +1,6 @@
 
-for(var i = 0; i < 10; i++) {
-    viewer.addLabel("Hello "+i,{position:{x:i,y:0,z:i}, backgroundColor: 'orange',frame:i});
+for(let i = 0; i < 10; i++) {
+    viewer.addLabel(`Hello ${i}`,{position:{x:i,y:0,z:i}, backgroundColor: 'orange',frame:i});
 }
 
 viewer.animate({loop:"forward",reps:1});
