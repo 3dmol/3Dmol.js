0700c92ab268eed2f416703a3e45c1f110338d09
Zirui Huang <472383277@qq.com>
2022-03-18 18:04:17 -0400

    changed to +=1

--- tests/auto/test.js
+++ tests/auto/test.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
+$(document).ready(()=> {
     window.devicePixelRatio = 2.001; // for compat with headless
-    var getKeys = function(obj){
-       var keys = [];
-       for(var key in obj){
+    const getKeys = function(obj){
+       const keys = [];
+       for(const key in obj){
           keys.push(key);
        }
        return keys;
     }
 
-    var canvasCount= function(){
+    const canvasCount= function(){
 
         return $('#gldiv').children().length;
     }
@@ -16,18 +16,18 @@ $(document).ready(function(){
     /**
     Tests for global vairiables 
     */
-    var GlobalTester = (function(){
-        var fields={}
-        var before = function(w){
-            for(var field in w){
+    const GlobalTester = (function(){
+        const fields={}
+        const before = function(w){
+            for(const field in w){
                 fields[field] = true;
             }
             return fields;
         }
 
-        var after = function(w){
-            var errors=[];
-            for(var field in w){
+        const after = function(w){
+            const errors=[];
+            for(const field in w){
                 if(!fields[field]){
                     delete window[field];
                     errors.push(field);
@@ -36,14 +36,14 @@ $(document).ready(function(){
             return errors;
 
         }
-        return {before: before, after:after};
+        return {before, after};
     }());
 
     function waitfor(test, expectedValue, msec, count, source, callback) {
         // Check if condition met. If not, re-check later (msec).
         if (test() !== expectedValue) {
-            count++;
-            setTimeout(function() {
+            count+=1;
+            setTimeout(() => {
                 waitfor(test, expectedValue, msec, count, source, callback);
             }, msec);
             return;
@@ -54,45 +54,45 @@ $(document).ready(function(){
 
     function createRow(key){
 
-        var tr = $('<tr class="examplerow">').prop('id',key+"_row");
-        $('<td class="label">').appendTo(tr).append('<a class="testname">'+key+'</a>');
+        const tr = $('<tr class="examplerow">').prop('id',`${key}_row`);
+        $('<td class="label">').appendTo(tr).append(`<a class="testname">${key}</a>`);
         $('<td class="rendered">').appendTo(tr);
-        $('<td class="reference">').appendTo(tr).append('<img src="../glcheck/reference-images/tests-glcheck-render-tests-'+key+'.html.png">');
+        $('<td class="reference">').appendTo(tr).append(`<img src="../glcheck/reference-images/tests-glcheck-render-tests-${key}.html.png">`);
         $('<td class="difference">').appendTo(tr);
         $('#tests_table').append(tr);
         return tr;
     }
 
-    $('#sorttable').click(function() {
-        //sort the table with the worse examples first
-        //detach rows from tbody
-        var rows = $('#tests_table tr').detach().get();
-        //sort
-        rows.sort(function(a, b) {
+    $('#sorttable').click(() => {
+        // sort the table with the worse examples first
+        // detach rows from tbody
+        const rows = $('#tests_table tr').detach().get();
+        // sort
+        rows.sort((a, b) => {
 
-            var A = $(a).find('.percentage').html();
-            var B = $(b).find('.percentage').html();
+            let A = $(a).find('.percentage').html();
+            let B = $(b).find('.percentage').html();
             A = parseFloat(A);
             B = parseFloat(B);
-            if(isNaN(A)) A = 10000; //error messages go to front
+            if(isNaN(A)) A = 10000; // error messages go to front
             if(isNaN(B)) B = 10000;
-            return B-A; //large to small
+            return B-A; // large to small
 
         });
 
-        //reattach
+        // reattach
         $('#tests_table').append(rows);
        
     });
     
-    var keys=getKeys(system)
-    keys.sort(); //lexographic ordering
+    const keys=getKeys(system)
+    keys.sort(); // lexographic ordering
 
-    var par=$('<p>   0/'+keys.length+'</p>').css('display','inline');
+    const par=$(`<p>   0/${keys.length}</p>`).css('display','inline');
     $("#summary_list").append(par);
 
-    var beforeGlobals;
-    var i=0;
+    let beforeGlobals;
+    const i=0;
     $('#gldiv').hide();
 
 
@@ -126,13 +126,13 @@ $(document).ready(function(){
         */
     }
 
-    var beginTime = Date.now();
-    var failures = 0;
+    const beginTime = Date.now();
+    let failures = 0;
     resemble.outputSettings({
         useCrossOrigin: false
     });
     
-    //https://stackoverflow.com/questions/19327749/javascript-blob-filename-without-link
+    // https://stackoverflow.com/questions/19327749/javascript-blob-filename-without-link
     function saveFile(blob, filename) {
       if (window.navigator.msSaveOrOpenBlob) {
         window.navigator.msSaveOrOpenBlob(blob, filename);
@@ -151,112 +151,112 @@ $(document).ready(function(){
     }
     
     function runTest(i){
-        if(i == keys.length) {
-            //finished
-            var endTime = Date.now();
-            $('#summary_list').append('<li class="totaltime">Total time: '+(endTime-beginTime)/1000+'s'+'</p>');
-            $('#summary_list').append('<li class="failures">Total failures: '+failures+'</p>');
+        if(i === keys.length) {
+            // finished
+            const endTime = Date.now();
+            $('#summary_list').append(`<li class="totaltime">Total time: ${(endTime-beginTime)/1000}s`+`</p>`);
+            $('#summary_list').append(`<li class="failures">Total failures: ${failures}</p>`);
             
             zip.generateAsync({type:'blob'})
-                .then(function(content) {
+                .then((content) => {
                     $('#download').prop('disabled',false);
-                    $('#download').click(function() {
+                    $('#download').click(() => {
                         saveFile(content,'images.zip');
                     });
                 });
             return;
         }
-        console.log("%c-------------------------- "+i+": "+keys[i]+" -----------------------------",'background: green; color: white; display: block;')
-        var before=Date.now();
-        var key=keys[i];
+        console.log(`%c-------------------------- ${i}: ${keys[i]} -----------------------------`,'background: green; color: white; display: block;')
+        const before=Date.now();
+        const key=keys[i];
         
-        var viewer=$3Dmol.createViewer($("#gldiv"),{id:key});
-        var afterGlobals;
-        var nexti = i+1; //just in case exception happens after i increment
+        const viewer=$3Dmol.createViewer($("#gldiv"),{id:key});
+        let afterGlobals;
+        const nexti = i+1; // just in case exception happens after i increment
 
-        //create the table row
-        var tableRow=createRow(key);
-        var percentage=$('<p class="percentage">');
+        // create the table row
+        const tableRow=createRow(key);
+        const percentage=$('<p class="percentage">');
         tableRow.find(".label").append(percentage);
         $('#tests').find('tbody').append(tableRow);
         
-        par.html("   "+(i+1)+"/"+keys.length);
+        par.html(`   ${i+1}/${keys.length}`);
 
-        //setup error handling
-        var setError = function(msg) {
-            var listElement=$('<li class="erroritem">').append('<a href="#'+key+'_row">'+key+' '+msg+'</a>');
+        // setup error handling
+        const setError = function(msg) {
+            const listElement=$('<li class="erroritem">').append(`<a href="#${key}_row">${key} ${msg}</a>`);
             tableRow.find('.label').css('backgroundColor','red');
             $('#summary_list').append(listElement)
             percentage.html(msg);
-            failures++;
+            failures+=1;
         }
         
-        //override default error handling (try/catch isn't sufficient due to callbacks)
+        // override default error handling (try/catch isn't sufficient due to callbacks)
         window.onerror = function(message, file, lineNumber) {
             setError(message);
-            failures++;
-            setTimeout(function() {runTest(nexti);}, 1); //let page update  
+            failures+=1;
+            setTimeout(() => {runTest(nexti);}, 1); // let page update  
             return true; 
           };
           
-        system[key](viewer,function(viewer){
-            waitfor(function() { return viewer.surfacesFinished() && !viewer.isAnimated() } , true , 1000 , 0 , "" , function(){
-                var after=Date.now();
-                //gets the canvas
-                var canvas=viewer.getCanvas(); //$("canvas#"+key).get(0);
-                //creates an image for the canvas
-                var canvasImageData = imageFromWebGlCanvas(canvas);
-                zip.file(key+'.png',canvasImageData.split('base64,')[1], {base64: true});
-                var canvasImage=$("<img name="+key+" class='renderedImage'>").attr('src',canvasImageData);
+        system[key](viewer,(viewer)=> {
+            waitfor(() => viewer.surfacesFinished() && !viewer.isAnimated() , true , 1000 , 0 , "" , ()=> {
+                const after=Date.now();
+                // gets the canvas
+                const canvas=viewer.getCanvas(); // $("canvas#"+key).get(0);
+                // creates an image for the canvas
+                const canvasImageData = imageFromWebGlCanvas(canvas);
+                zip.file(`${key}.png`,canvasImageData.split('base64,')[1], {base64: true});
+                const canvasImage=$(`<img name=${key} class='renderedImage'>`).attr('src',canvasImageData);
 
-                //click event for canvas
-                canvasImage.click(function(){
-                    var win = window.open();
-                    win.location="generate_test.cgi?test="+key;
+                // click event for canvas
+                canvasImage.click(()=> {
+                    const win = window.open();
+                    win.location=`generate_test.cgi?test=${key}`;
                 });
                 tableRow.find('.rendered').append(canvasImage);
                 tableRow.find('a.testname').attr('href',canvasImageData);
-                tableRow.find('a.testname').attr('download',key+".png");
+                tableRow.find('a.testname').attr('download',`${key}.png`);
 
-                var differenceImage=$('<img>');
-                var differ=0;
+                const differenceImage=$('<img>');
+                let differ=0;
 
-                var diff = resemble(canvasImageData).compareTo("../glcheck/reference-images/tests-glcheck-render-tests-"+key+".html.png").set3DmolTolerances().scaleToSameSize().onComplete(function(data){
-                    //scaletosamesize is necessary for retina displays
+                const diff = resemble(canvasImageData).compareTo(`../glcheck/reference-images/tests-glcheck-render-tests-${key}.html.png`).set3DmolTolerances().scaleToSameSize().onComplete((data)=> {
+                    // scaletosamesize is necessary for retina displays
                     if(data.error) {
-                        throw data.error; //probably need to create an image file
+                        throw data.error; // probably need to create an image file
                     }
-                    differ=data.rawMisMatchPercentage;//(100-blankDiff);
-                    percentage.html(differ+'<br>'+(after-before)+'ms');
+                    differ=data.rawMisMatchPercentage;// (100-blankDiff);
+                    percentage.html(`${differ}<br>${after-before}ms`);
                     differenceImage.attr('src',data.getImageDataUrl());
                     tableRow.find('.difference').append(differenceImage);
                     
-                    //compare globals before and after
+                    // compare globals before and after
                     afterGlobals=GlobalTester.after(window);
                     
                     if(afterGlobals.length > 0) {
-                        setError("Globals added: "+afterGlobals);
+                        setError(`Globals added: ${afterGlobals}`);
                     } else if(differ>5){
                         setError(differ);
                     } else{
                         tableRow.find(".label").css('backgroundColor',"green");
                     }
                     
-                    //remove possible div
+                    // remove possible div
                     $(".viewer_3Dmoljs").remove();
-                    //remove canvas
+                    // remove canvas
                     $(canvas).remove();
                     
-                    setTimeout(function() {runTest(nexti);}, 1); //let page update             
-                }); //end onComplete
+                    setTimeout(() => {runTest(nexti);}, 1); // let page update             
+                }); // end onComplete
             });
             
         });
             
     }    
     
-    //initialize a viewer since jquery adds some event handling stuff to the window
-    //that we don't want to caught by the global tester
+    // initialize a viewer since jquery adds some event handling stuff to the window
+    // that we don't want to caught by the global tester
     $3Dmol.createViewer($("#gldiv"));    
     zip = new JSZip();
     GlobalTester.before(window);     
