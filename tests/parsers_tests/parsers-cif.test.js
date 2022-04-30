9635672a6cfc31f3c29f662c0e0ddc9c0678f271
Zirui Huang <472383277@qq.com>
2022-04-30 01:16:26 -0400

    changed import path

--- tests/parsers_tests/parsers-cif.test.js
+++ tests/parsers_tests/parsers-cif.test.js
@@ -4,7 +4,7 @@
 
 global.$ = require("jquery");
 global.URL.createObjectURL = function() {};
-let $3Dmol = require("../build/3Dmol.js");
+let $3Dmol = require("../../build/3Dmol.js");
 const fs = require('fs');
 
 describe('Function cif | Input: multiple.cif | option: assignBonds |', ()=>{
