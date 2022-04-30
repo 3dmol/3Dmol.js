b3b1723f053a213c69db4fbf36ed6effccf6c943
Zirui Huang <472383277@qq.com>
2022-04-30 01:14:59 -0400

    changed test input

--- tests/parsers_tests/parsers-pdbqt.test.js
+++ tests/parsers_tests/parsers-pdbqt.test.js
@@ -7,13 +7,9 @@ global.URL.createObjectURL = function() {};
 let $3Dmol = require("../../build/3Dmol.js");
 const fs = require('fs');
 
-describe('Function PDBQT | Input:1B5S.pdb |', ()=>{
-    const data = fs.readFileSync('tests/test_structs/1B5S.pdb', 'utf-8')
+describe('Function PDBQT | Input:1SMT.pdb |', ()=>{
+    const data = fs.readFileSync('tests/test_structs/1SMT.pdb', 'utf-8')
     let atoms = $3Dmol.Parsers.PDBQT(data, {});
-
-    test("Length of atoms should be 1", ()=>{
-        expect(atoms.length).toBe(1);
-    });
 
     test("Atoms should match the snapshot", ()=>{
         expect(atoms).toMatchSnapshot();
@@ -21,8 +17,8 @@ describe('Function PDBQT | Input:1B5S.pdb |', ()=>{
 
 });
 
-describe('Function PDBQT | Input:1B5S.pdb | option: multimodel, onemol |', ()=>{
-    const data = fs.readFileSync('tests/test_structs/1B5S.pdb', 'utf-8')
+describe('Function PDBQT | Input:1SMT.pdb | option: multimodel, onemol |', ()=>{
+    const data = fs.readFileSync('tests/test_structs/1SMT.pdb', 'utf-8')
     
     test("Atoms should match the snapshot (multimodel:true, onemol:false/undefined)", ()=>{
         let atoms = $3Dmol.Parsers.PDBQT(data, {multimodel:true, onemol:false});
