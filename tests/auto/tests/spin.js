d84e9179cf6d134f252b70bd2cb6c985f4b12e84
David Koes <dkoes@pitt.edu>
2023-02-18 21:02:03 -0500

    make timing of spin test more consistent

--- tests/auto/tests/spin.js
+++ tests/auto/tests/spin.js
@@ -1,16 +1,16 @@
 
-var cnt = 0;
-viewer.setViewChangeCallback(() => {
-   cnt += 1;
-   if(cnt == 50) {
-      viewer.spin(false);
-      viewer.render();
-   }
-});
 $.get('data/1lo6.pdb', function(data) {
       viewer.addModel(data,'pdb');
       viewer.setStyle({cartoon:{color:'spectrum'}});
       viewer.zoomTo();
       viewer.spin('z');
       viewer.render( );
+      var cnt = 0;
+      viewer.setViewChangeCallback(() => {
+         cnt += 1;
+         if(cnt == 47) {
+            viewer.spin(false);
+            viewer.render();
+         }
+      });      
 });
