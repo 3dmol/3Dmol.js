8c2d1009f863ac137cd9acd40fbf687f4b14e7c8
Adithya Krishna <adikrish@redhat.com>
2023-03-23 21:39:06 +0530

    Added Export for Constants to Index Files
    Signed-off-by: Adithya Krishna <adikrish@redhat.com>

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,17 @@ export * from "./parsers";
 export * from "./WebGL/math";
 export * from "./WebGL/shapes";
 export * from "./utilities";
-export * from "./ProteinSurface4"
-export * from "./VolumeData"
-export * from "./VolumetricRender"
-export * from "./GLShape"
-export * from "./GLDraw"
-export * from "./glcartoon"
-export * from "./GLModel"
-export * from "./GLViewer"
-export * from "./autoload"
-export * from "./specs"
+export * from "./ProteinSurface4";
+export * from "./VolumeData";
+export * from "./VolumetricRender";
+export * from "./GLShape";
+export * from "./GLDraw";
+export * from "./glcartoon";
+export * from "./GLModel";
+export * from "./GLViewer";
+export * from "./autoload";
+export * from "./specs";
+export * from "./Constants";
 
 //import * as $ from 'jquery';
 declare var __webpack_exports__: any;
