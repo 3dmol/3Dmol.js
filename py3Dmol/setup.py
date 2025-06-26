f393cdeb4f3c44cee1066cfeedd4344e2ec776ce
David Koes <dkoes@pitt.edu>
2025-06-26 11:23:11 -0400

    version bump

--- py3Dmol/setup.py
+++ py3Dmol/setup.py
@@ -24,7 +24,7 @@ setup(
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
     # Keep version in synce with 3Dmol.js version.  Use "postX" suffix if needed
-    version='2.5.0',
+    version='2.5.1',
 
     description='An IPython interface for embedding 3Dmol.js views in Jupyter notebooks',
     long_description=long_description,
